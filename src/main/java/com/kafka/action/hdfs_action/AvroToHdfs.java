5b706976a5a6b26ff374c7f4893479e98bac1b9d
lg <lg@DESKTOP-NVAQ9FO.lan>
2019-01-27 00:54:47 +0800

    Avro2Hdfs--day1

--- src/main/java/com/kafka/action/hdfs_action/AvroToHdfs.java
+++ src/main/java/com/kafka/action/hdfs_action/AvroToHdfs.java
@@ -1,13 +1,10 @@
-package com.kafka.action.kafka_action;
+package com.kafka.action.hdfs_action;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.avro.Schema;
@@ -17,43 +14,28 @@ import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.DatumWriter;
-import org.apache.avro.util.internal.JacksonUtils;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.Path;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.JSONPath;
 import com.alibaba.fastjson.parser.Feature;
 import com.kafka.action.util.ConvertDateType;
 
-public class TestRecordJson {
+public class AvroToHdfs {
 
-	public static void main(String[] args) throws IOException {
+	public static void avroSchema(Path path,ConsumerRecord<String, String> msg,FSDataOutputStream outputStream) throws IOException {
 
-//		String str = "{\r\n" + "    \"table\": \"DBO.TAB\", \r\n" + "    \"op_type\": \"I\", \r\n"
-//				+ "    \"op_ts\": \"2019-01-21 14:05:18.446967\", \r\n"
-//				+ "    \"current_ts\": \"2019-01-21T22:26:10.481001\", \r\n"
-//				+ "    \"pos\": \"00000000400000050285\", \r\n" + "    \"primary_keys\": [\r\n" + "        \"ID\"\r\n"
-//				+ "    ], \r\n" + "    \"after\": {\r\n" + "        \"ID\": 211170, \r\n"
-//				+ "        \"BIRTHDATE\": \"2019-01-22 00:58:17.903000000\", \r\n" + "        \"AGE\": 99, \r\n"
-//				+ "        \"NAME\": \"kkrrr\"\r\n" + "    }\r\n" + "}\r\n";
-		String str = "{\r\n" + "    \"table\": \"DBO.TAB\", \r\n" + "    \"op_type\": \"U\", \r\n"
-				+ "    \"op_ts\": \"2019-01-21 14:05:18.446967\", \r\n"
-				+ "    \"current_ts\": \"2019-01-21T22:26:10.481001\", \r\n"
-				+ "    \"pos\": \"00000000400000050285\", \r\n" + "    \"primary_keys\": [\r\n" + "        \"ID\"\r\n"
-				+ "    ], \r\n" + "    \"before\": {\r\n" + "        \"ID\": 211170, \r\n"
-				+ "        \"BIRTHDATE\": \"2019-01-22 00:58:17.903000000\", \r\n" + "        \"AGE\": 99, \r\n"
-				+ "        \"NAME\": \"kkrrr\"\r\n" + "    }, \r\n" + "    \"after\": {\r\n"
-				+ "        \"ID\": 211171, \r\n" + "        \"BIRTHDATE\": \"2019-01-22 00:58:17.903000000\", \r\n"
-				+ "        \"AGE\": 88, \r\n" + "        \"NAME\": \"kkrrr\"\r\n" + "    }\r\n" + "}";
-		System.out.println(str);
-		Object json = JSONArray.parse(str);
+		String msg_value = msg.value();
+		Object json = JSONArray.parse(msg_value);
 		String current_ts = ((String) JSONPath.eval(json, "$.current_ts")).replace("T", " ");
 		String tab = ((String) JSONPath.eval(json, "$.table")).replace("DBO.", "");
 		Object primary_keys = (Object) JSONPath.eval(json, "$.primary_keys");
 		String op_type = (String) JSONPath.eval(json, "$.op_type");
 		Object before = (Object) JSONPath.eval(json, "$.before");
-		Object after = (Object) JSONPath.eval(json, "$.after");
+		Object after = (Object) JSONPath.eval(json, "$.after");	
 		Map<String, Object> mapafter = new HashMap<String, Object>();
 		Map<String, Object> mapbefore = new HashMap<String, Object>();
 		if (after != null && after != "") {
@@ -74,9 +56,9 @@ public class TestRecordJson {
 			Object pk_after = null;
 			String primary_keystr = primary_keys.toString();
 			Pattern p = Pattern.compile("\\[|\\]|\"");
-			String[] pkarr = p.matcher(primary_keystr).replaceAll("").split(",");
+			String[] pkarray = p.matcher(primary_keystr).replaceAll("").split(",");
 			Boolean flag = false;
-			for (String primary_key : pkarr) {
+			for (String primary_key : pkarray) {
 				pk_before = (Object) JSONPath.eval(before, "$." + primary_key + "");
 				pk_after = (Object) JSONPath.eval(after, "$." + primary_key + "");
 				if (pk_before.equals(pk_after)) {
@@ -90,6 +72,7 @@ public class TestRecordJson {
 
 			if (!flag) {
 				mapbefore.put("ISDELETED", 1);
+				
 			}
 
 		}
@@ -98,7 +81,6 @@ public class TestRecordJson {
 			mapafter.put("ISDELETED", 1);
 		}
 
-		System.out.println(JSON.toJSONString(mapafter));
 
 		StringBuilder tableschema = new StringBuilder();
 		tableschema = tableschema.append("{\"namespace\": \"com.kafka.action.chapter6.avro\",\r\n"
@@ -113,16 +95,13 @@ public class TestRecordJson {
 		}
 		tableschema.deleteCharAt(tableschema.length() - 2);
 		tableschema.append(" ]\r\n" + "}\r\n" + "");
-		System.out.println(tableschema.toString());
 
 		Schema schema = new Schema.Parser().parse(tableschema.toString());
 		GenericRecord table = new GenericData.Record(schema);
 
 		DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<GenericRecord>(schema);
 		DataFileWriter<GenericRecord> writer = new DataFileWriter(datumWriter).setCodec(CodecFactory.snappyCodec());
-
-		File avro = new File("D:\\Test.Avro");
-		writer.create(schema, avro);
+		writer.create(schema, outputStream);
 		if (mapafter.size() > 0) {
 			for (Entry<String, Object> entry : mapafter.entrySet()) {
 				table.put(entry.getKey(), entry.getValue());
@@ -134,9 +113,8 @@ public class TestRecordJson {
 				table.put(entry.getKey(), entry.getValue());
 			}
 		}
-		
 		writer.append(table);
-		writer.close();
+		writer.close();	
 	}
 
 }
--- src/main/java/com/kafka/action/hdfs_action/WriteToHdfs.java
+++ src/main/java/com/kafka/action/hdfs_action/WriteToHdfs.java
@@ -2,6 +2,7 @@ package com.kafka.action.hdfs_action;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
@@ -14,6 +15,12 @@ import com.kafka.action.util.ConfigUtil;
 import com.kafka.action.util.SystemEnum;
 
 public class WriteToHdfs {
+	private static Properties HDFSPROP = null;
+	private static String fileDocFormat = null;
+	static {
+		HDFSPROP = ConfigUtil.getProperties(SystemEnum.HDFS);
+		fileDocFormat = HDFSPROP.getProperty("fileDocFormat");
+	}
 
 	public static Boolean writeData(Path path, List<ConsumerRecord<String, String>> msgs) {
 		Boolean rs = false;
@@ -24,8 +31,11 @@ public class WriteToHdfs {
 				Configuration conf = ConfigUtil.getConfiguration(ConfigUtil.getProperties(SystemEnum.HDFS));
 				fs = FileSystem.get(conf);
 				outputStream = fs.append(path);
-
-				outputStream.write(msgs.toString().getBytes("utf-8"));
+				if (fileDocFormat.equals("avro")) {
+					 for(ConsumerRecord<String, String> msg :msgs) {
+						 AvroToHdfs.avroSchema(path,msg ,outputStream);
+					 }
+				}
 				fs.close();
 				outputStream.close();
 				rs = true;
